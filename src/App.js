3077db2fec10ebcf1745e3a4e6bd43c9b71c6dc1
wzd200 <william.z.dye@gmail.com>
2022-02-13 12:27:04 -0500

    navbar styled, hamburger menu imported from fontawesome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
     <div>
-      <h2>Portfolio Project</h2>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+export const Navbar = () => {
+  return (
+    <div>
+        <nav className='navbar navbar-expand-lg navbar-light bg-dark'>
+            <div className='container'>
+                <a className='navbar-brand' href='#'>William Dye</a>
+                <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+                    {/* <span className='navbar-toggler-icon'></span> */}
+                    <FontAwesomeIcon icon={faBars} style={{ color: '#fff'}}/>
+                </button>
+
+                <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+                    <ul className='navbar-nav ms-auto'>
+                        <li className='nav-item active'>
+                            <a className='nav-link' href='#'>Home 
+                                {/* <span className='sr-only'>(current)</span> */}
+                            </a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link' href='#'>About Me</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link' href='#'>Services</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link' href='#'>How I Work</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link' href='#'>Portfolio</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link' href='#'>Contact Me</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+  )
+}
