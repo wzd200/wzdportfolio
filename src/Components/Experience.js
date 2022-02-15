0f96ca88e6fb081df508324481d1337f9f09cbbc
wzd200 <william.z.dye@gmail.com>
2022-02-15 16:11:22 -0500

    added experience text

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -10,40 +10,40 @@ const Experience = () => {
             <div className='timeline-block timeline-block-right'>
                 <div className='marker'></div>
                 <div className='timeline-content'>
-                    <h3>2020-2021</h3>
-                    <p>The soft body can radically alter its shape, enabling octopuses to squeeze through small gaps. They trail their eight appendages behind them as they swim. The siphon is used both for respiration and for locomotion, by expelling a jet of water. Octopuses have a complex nervous system and excellent sight, and are among the most intelligent and behaviourally diverse of all invertebrates.</p>
+                    <h3>2020</h3>
+                    <p>I began my coding journey at the Bloomtech Institute of Technology. During my time there, I worked on a few Bloomtech Institute of Technology build week projects where groups of students were tasked with creating an application in a week's time and gained valuable front-end and back-end experience using React, Redux, Node.js, Express, and Python.</p>
                 </div>
             </div>
             {/* - */}
             <div className='timeline-block timeline-block-left'>
                 <div className='marker'></div>
                 <div className='timeline-content'>
-                    <h3>2020-2021</h3>
-                    <p>The soft body can radically alter its shape, enabling octopuses to squeeze through small gaps. They trail their eight appendages behind them as they swim. The siphon is used both for respiration and for locomotion, by expelling a jet of water. Octopuses have a complex nervous system and excellent sight, and are among the most intelligent and behaviourally diverse of all invertebrates.</p>
+                    <h3>2020</h3>
+                    <p>I worked on a prototype website for an aspiring restaurateur. I took this project on early during my time at Bloomtech Institute of Technology, hoping to apply what I was learning at the time to make custom components using React and CSS.</p>
                 </div>
             </div>
             {/* - */}
             <div className='timeline-block timeline-block-right'>
                 <div className='marker'></div>
                 <div className='timeline-content'>
-                    <h3>2020-2021</h3>
-                    <p>The soft body can radically alter its shape, enabling octopuses to squeeze through small gaps. They trail their eight appendages behind them as they swim. The siphon is used both for respiration and for locomotion, by expelling a jet of water. Octopuses have a complex nervous system and excellent sight, and are among the most intelligent and behaviourally diverse of all invertebrates.</p>
+                    <h3>2021</h3>
+                    <p>I participated in the Bloomtech Institute of Technology's labs program, where I was selected as a backend engineer for a project that built for, and ultimately shipped to the Human Right's First association. The goal of the project was to build an application that would assist lawyers working asylum cases by visualizing judge's trends with sanctioning asylum based on specific arguments made in court.</p>
                 </div>
             </div>
             {/* - */}
             <div className='timeline-block timeline-block-left'>
                 <div className='marker'></div>
                 <div className='timeline-content'>
-                    <h3>2020-2021</h3>
-                    <p>The soft body can radically alter its shape, enabling octopuses to squeeze through small gaps. They trail their eight appendages behind them as they swim. The siphon is used both for respiration and for locomotion, by expelling a jet of water. Octopuses have a complex nervous system and excellent sight, and are among the most intelligent and behaviourally diverse of all invertebrates.</p>
+                    <h3>2021</h3>
+                    <p>The second Bloomtech Institute of Technology's labs project I worked on was the Underdog Devs hub application. The goal of this project was to create a hub for the Underdog Devs volunteer mentor/mentee program. The hub would play a large role in matching mentees to mentors and facilitating their resources.</p>
                 </div>
             </div>
             {/* - */}
             <div className='timeline-block timeline-block-right'>
                 <div className='marker'></div>
                 <div className='timeline-content'>
-                    <h3>2020-2021</h3>
-                    <p>The soft body can radically alter its shape, enabling octopuses to squeeze through small gaps. They trail their eight appendages behind them as they swim. The siphon is used both for respiration and for locomotion, by expelling a jet of water. Octopuses have a complex nervous system and excellent sight, and are among the most intelligent and behaviourally diverse of all invertebrates.</p>
+                    <h3>2021 - Present</h3>
+                    <p>I graduated from Bloomtech Institute of Technology's full-stack web-development program. Currently, I am working through a Udemy course whilst searching for my next job.</p>
                 </div>
             </div>
             {/* - */}
