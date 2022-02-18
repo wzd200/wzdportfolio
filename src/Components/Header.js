9b73dd7ee4f09227a787d46cf37afec085f5f5fa
wzd200 <william.z.dye@gmail.com>
2022-02-18 11:28:09 -0500

    added header contact me button functionality

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Typed from 'react-typed';
+import { Link } from 'react-scroll';
 
 const Header = () => {
   return (
@@ -15,7 +16,7 @@ const Header = () => {
                 backSpeed={60}
                 loop
             />
-            <a href='a' className='btn-main-offer'>Contact Me</a>
+            <Link href='/' smooth={true} className='btn-main-offer' to='contact' offset={-70}>Contact Me</Link>
         </div>
     </div>
   )
